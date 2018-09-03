718491d25c5bff15ef994e46f7141b9789d4297d
Osama <osamabinjunaid36@gmail.com>
2018-09-04 01:22:18 +0530

    repaired broken build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let a = document.querySelector('#jmi');
 var initial = 62698;
 
 function changer(){
-    data += 1;
+    initial += 1;
     a.href=`http://13.250.148.144/jasperserver/flow.html?_flowId=viewReportFlow&standAlone=true&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fgradecard_jmi&reportUnit=%2Freports%2Fgradecard_jmi%2Fgradecard12_extra&j_username=jasperadmin&j_password=jasperadmin&output=pdf&trcoursecode=TR00003168&frmslno=${initial}&toslno=${initial}`;
     console.log(data);
 }
