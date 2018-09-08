d2564c9e4986f4dbc796d31d19aea8363105ce76
Osama <osamabinjunaid36@gmail.com>
2018-09-08 15:00:27 +0530

    added prompt functionality to app.js && minor changes to index.html

--- app.js
+++ app.js
@@ -13,10 +13,20 @@ function changer(){
     _url=`http://13.250.148.144/jasperserver/flow.html?_flowId=viewReportFlow&standAlone=true&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fgradecard_jmi&reportUnit=%2Freports%2Fgradecard_jmi%2Fgradecard12_extra&j_username=jasperadmin&j_password=jasperadmin&output=pdf&trcoursecode=TR00003168&frmslno=${initial}&toslno=${initial}`;
     window.open(_url,'_blank');
 }
-
-while(initial < 63100){
-    changer();
-    console.log(initial);
+function exec(){
+    alert('This will this Will open 100 tabs at once in your browser !!');
+    input = prompt("Do You want to Continue type (Y/N)");
+    if (input.toUpperCase() == 'N'){
+        return;
+    }
+    else
+    {
+        while(initial < 63100){
+         changer();
+         console.log(initial);
+     }
+     console.log('Executed SucessFully');
+     }
 }
+$('a').on('click',exec);
 
-console.log('Executed SucessFully');
